e3298a272f7de9b7d463a52216ccfd8eff0e0b96
maomaodan <morgan20050135@gmail.com>
2015-10-11 01:05:10 -0500

    implemented listview with sharedpreference object completed InputName activity

--- app/src/main/java/edu/wisc/ece/ece454minilab5/InputName.java
+++ app/src/main/java/edu/wisc/ece/ece454minilab5/InputName.java
@@ -1,11 +1,17 @@
 package edu.wisc.ece.ece454minilab5;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class InputName extends AppCompatActivity {
+
+    private String name;
+    SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,15 +19,29 @@ public class InputName extends AppCompatActivity {
         setContentView(R.layout.activity_input_name);
 
 
+
+
+
     }
+    public void onGoBackSave(View v)
+    {
+        mPrefs = getSharedPreferences(ListViewLayoutActivity.PREF_NAME, MODE_PRIVATE);
+        TextView txt = (TextView)findViewById(R.id.editText);
+        name = txt.getText().toString();
+        SharedPreferences.Editor editor = mPrefs.edit();
 
-    public void goBackSave(View v) {
-
-
+        ListViewLayoutActivity.ID++;
+        editor.putString(Integer.toString(ListViewLayoutActivity.ID), name);
+        editor.commit();
 
 
 
         Intent intent = new Intent(InputName.this, ListViewLayoutActivity.class);
+        startActivity(intent);
     }
-
+    public void onGoBackCancel(View v)
+    {
+        Intent intent = new Intent(InputName.this, ListViewLayoutActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/edu/wisc/ece/ece454minilab5/ListViewLayoutActivity.java
+++ app/src/main/java/edu/wisc/ece/ece454minilab5/ListViewLayoutActivity.java
@@ -1,31 +1,46 @@
 package edu.wisc.ece.ece454minilab5;
 
-import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class ListViewLayoutActivity extends ListActivity {
-
+    protected static int ID;
     private ArrayAdapter<String> mAdapter;
-    protected ArrayList<String> FOLKS = new ArrayList<String>(Arrays.asList("add","studentA", "studentB", "studentC", "studentD"));
-
+    ArrayList<String> nameArray = new ArrayList<String>();
+    protected SharedPreferences namePrefs;
+    //protected ArrayList<String> FOLKS = new ArrayList<String>(Arrays.asList("add","studentA", "studentB", "studentC", "studentD"));
+    public static final String PREF_NAME = "NAME";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         // Define a new adapter
+
+
+        namePrefs =getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        HashMap <Integer, String> nameMap = (HashMap)namePrefs.getAll();
+        nameArray.add("add");
+        ID = nameMap.size();
+        for (int i = 1; i <= 10;i++)
+        {
+            if(nameMap.get(Integer.toString(i))!=null)
+            nameArray.add(nameMap.get(Integer.toString(i)));
+        }
+
         mAdapter = new ArrayAdapter<String>(this,
-                R.layout.activity_list_view_layout, FOLKS);
+                R.layout.activity_list_view_layout, nameArray);
 
         // Assign the adapter to ListView
         setListAdapter(mAdapter);
@@ -35,14 +50,22 @@ public class ListViewLayoutActivity extends ListActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 //Your code here...
-                Toast.makeText(ListViewLayoutActivity.this,FOLKS.get((int)id)+" is removed form the list!",Toast.LENGTH_SHORT).show();
+
                 if (id==0)
                 {
-
+                    Intent intent = new Intent (ListViewLayoutActivity.this,InputName.class);
+                    startActivity(intent);
                 }
                 else
-                    FOLKS.remove((int)id);
+                {
+                    Toast.makeText(ListViewLayoutActivity.this,nameArray.get((int)id)+" is removed form the list!",Toast.LENGTH_SHORT).show();
+                    nameArray.remove((int) id);
+                    SharedPreferences.Editor editor = namePrefs.edit();
 
+                    editor.remove(Long.toString(id));
+                    editor.commit();
+                    ID--;
+                }
                 mAdapter.notifyDataSetChanged();
 
             }
